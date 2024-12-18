ab462f0be0aa891cef7eb05620a5c5e8ccecbcb2
Sam White <sam.white@quantinuum.com>
2024-12-18 10:15:11 +0000

    fix: support valid types for compiler options

--- tests/models/test_backend_config.py
+++ tests/models/test_backend_config.py
@@ -1,6 +1,33 @@
-from quantinuum_schemas.models.backend_config import AerConfig
+from quantinuum_schemas.models.backend_config import AerConfig, QuantinuumCompilerOptions
+from pydantic import ValidationError
+
+import pytest
+
 
 
 def test_instantiation() -> None:
     aer_config = AerConfig()
     assert isinstance(aer_config, AerConfig)
+
+def test_valid_quantinuum_compiler_options() -> None:
+    """Test to ensure that all expected arguments can be accepted by the compiler options class"""
+    dict_of_options = {
+        "expect_threshold": 0.5,
+        "DD_threshold_times": [0.1, 0.2, 0.3],
+        "CF": "non linear",
+        "test_cz": False,
+        "max_planning": 601
+
+    }
+
+    QuantinuumCompilerOptions(**dict_of_options)
+
+def test_handling_invalid_option() -> None:
+    """Expect an assert error raised when passing a bad compiler option"""
+    dict_of_options = {
+        "DD_threshold_times": [0.1, 3, 0.3],
+    }
+
+    with pytest.raises(ValidationError): 
+        QuantinuumCompilerOptions(**dict_of_options)
+    
